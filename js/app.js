cba9d499d15e59cd867e011a021e4a5072feb248
nivruttigithub <nivruttiwagh2002@gmail.com>
2026-06-11 17:39:56 +0530

    [Nivrutti]added. Category and Operation Selection

--- js/app.js
+++ js/app.js
@@ -1,3 +1,83 @@
 console.log(
     "Quantity Measurement Frontend Started"
 );
+
+let selectedCategory = null;
+
+let selectedOperation = null;
+
+/*
+----------------------------------
+Category Selection
+----------------------------------
+*/
+
+const categoryCards =
+    document.querySelectorAll(
+        ".category-card"
+    );
+
+categoryCards.forEach(card => {
+
+    card.addEventListener(
+        "click",
+        () => {
+
+            categoryCards.forEach(c =>
+                c.classList.remove(
+                    "active"
+                )
+            );
+
+            card.classList.add(
+                "active"
+            );
+
+            selectedCategory =
+                card.dataset.category;
+
+            console.log(
+                "Selected Category:",
+                selectedCategory
+            );
+        }
+    );
+});
+
+/*
+----------------------------------
+Operation Selection
+----------------------------------
+*/
+
+const operationButtons =
+    document.querySelectorAll(
+        ".action-buttons button"
+    );
+
+operationButtons.forEach(button => {
+
+    button.addEventListener(
+        "click",
+        () => {
+
+            operationButtons.forEach(btn =>
+                btn.classList.remove(
+                    "active"
+                )
+            );
+
+            button.classList.add(
+                "active"
+            );
+
+            selectedOperation =
+                button.dataset.operation;
+
+            console.log(
+                "Selected Operation:",
+                selectedOperation
+            );
+        }
+    );
+});
